8b95fec2360697425c3e1dbabf63612427ed5a74
OrtegaChris <ortega_chris@outlook.com>
2014-12-09 20:55:22 -0500

    Revert "Revert "Scheduler""
    This reverts commit 1012b0d9c985b98655affbd3de655e85943affbe.

---
+++ Scheduler/src/startTimeComparator.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class startTimeComparator implements Comparator {
+
+    public int compare(Object p1, Object p2) {
+        int t1 = ((Process)p1).startTime;
+        int t2 = ((Process)p2).startTime;
+
+        if (t1 < t2){
+            return -1;
+        }
+        else{
+            return 1;
+        }
+    }
+
+}
